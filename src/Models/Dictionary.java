be1a46e63dbe08aec41cf4d1ac7ab823030b281a
Nguyenhuuthanh <thanhnh99.amc@github.com>
2018-10-05 16:43:19 +0700

    ttt

--- src/Models/Dictionary.java
+++ src/Models/Dictionary.java
@@ -1,17 +1,20 @@
 package Models;
 
+import Controller.DatabaseManagement;
+
 import java.util.Scanner;
 
 public class Dictionary {
-    private static final String EV_PATH = "/src/Res/dict_hh.db";
-    private DatabaseConnection EV_db = new DatabaseConnection(EV_PATH);
-
+    private static final String DICT_PATH = "/src/Res/dict_hh.db";
+    private DatabaseManagement DICT_db;
+    Dictionary(){
+        DICT_db = new DatabaseManagement(DICT_PATH);
+    }
     public static void main(String[] args){
         Dictionary dictionary = new Dictionary();
         String search;
         Scanner scanner = new Scanner(System.in);
         search = scanner.next();
-        /*Word test =*/ dictionary.EV_db.getWord(search, "av");
-        /*System.out.println(test.getWord_target() + "\n" + test.getWord_explain());*/
+
     }
 }
