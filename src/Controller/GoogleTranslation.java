47b48244234c692267c350a52bc0db83fce74180
Hue nguyen <hueahgase189@gmail.com>
2018-10-11 17:48:29 +0700

    update gg api

--- src/Controller/GoogleTranslation.java
+++ src/Controller/GoogleTranslation.java
@@ -1,15 +1,19 @@
 package Controller;
 
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GoogleTranslation {
-    public String translate(String input, String srcLang, String targetLang){
-        input = URLEncoder.encode((input));
-        String url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl="+srcLang+"&tl=" + targetLang + "&dt=t&q=" + input;
-        String result = Helper.HttpConnection.sendGet(url);
+    public String translate(String wordToTranslate, String srcLang, String targetLang){
+        wordToTranslate = wordToTranslate.replace(" ", "+");
+        String url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl="+srcLang+"&tl=" + targetLang + "&dt=t&q=" + wordToTranslate;
+        InputStream resultStream = Helper.HttpConnection.sendGet(url);
+        Scanner scanner = new Scanner(resultStream);
+        String result = scanner.nextLine();
         System.out.println(result);
         return result.substring(result.indexOf("[[[\"") + 4, result.indexOf("\",\""));
     }
