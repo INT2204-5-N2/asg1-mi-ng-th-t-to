98b2a8e5ab392ad05f5789b5710839dbfde48b0f
Nguyenhuuthanh <thanhnh99.amc@github.com>
2018-10-16 01:38:51 +0700

    update action Addwindow, Edit winow, Googletranslatewindow

--- src/Viewer/AddController.java
+++ src/Viewer/AddController.java
@@ -1,13 +1,19 @@
 package Viewer;
 
+import Controller.DatabaseManagement;
+import Models.Dictionary;
+import Models.Word;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class AddController implements Initializable {
@@ -19,6 +25,55 @@ public class AddController implements Initializable {
     private Button jbAdd;
     @FXML
     private Button jbCancel;
+    public  void CloseAddWindow(ActionEvent event)
+    {
+        Alert close =new Alert(Alert.AlertType.CONFIRMATION);
+        close.setTitle("Comfirmation close Window");
+        close.setHeaderText("Ban co chac chan");
+        close.setContentText("Thoát");
+        Optional<ButtonType> result= close.showAndWait();
+        ButtonType button=result.orElse(ButtonType.CANCEL);
+        if(button==ButtonType.OK)
+        {
+            System.out.println("Dong cua so");
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        else if(button==ButtonType.CANCEL)
+        {
+            System.out.println("Khong dong cua so nay");
+            close.close();
+        }
+    }
+
+    public void AddWord(ActionEvent event)
+    {
+        Alert Addwindow =new Alert(Alert.AlertType.CONFIRMATION);
+        Addwindow.setTitle("Confirmation Add Word ");
+        Addwindow.setHeaderText("Ban co muon them?");
+        Addwindow.setContentText(jtaEnglish.getText());
+        Optional<ButtonType> result= Addwindow.showAndWait();
+        ButtonType button=result.orElse(ButtonType.CANCEL);
+        if(button==ButtonType.OK)
+        {
+            System.out.println("Dong cua so");
+            Word newWord=new Word();
+            newWord.setWord_target(jtaEnglish.getText());
+            newWord.setWord_explain(jtaVietnamese.getText());
+            DatabaseManagement dbManagement = new DatabaseManagement(Dictionary.DICT_PATH);
+            dbManagement.addNewWord(newWord, Dictionary.DictType.ENG2VIET);
+//            Alert imformation=new Alert(Alert.AlertType.INFORMATION);
+//            imformation.setTitle("Add Word");
+//            imformation.setHeaderText("Thêm từ");
+//            imformation.setContentText("Thêm thành công");
+//            imformation.show();
+            System.out.println("Thêm thành công");
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        else
+        {
+            Addwindow.close();
+        }
+    }
     public void JtaEnglishGetText(ActionEvent e)
     {
         String a=jtaEnglish.getText();
--- src/Viewer/EditController.java
+++ src/Viewer/EditController.java
@@ -1,11 +1,38 @@
 package Viewer;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class EditController implements Initializable {
+
+    @FXML
+    private Button jbCancel;
+    public  void CloseEditWindow(ActionEvent event)
+    {
+        Alert close =new Alert(Alert.AlertType.INFORMATION);
+        close.setContentText("Thoát");
+        Optional<ButtonType> result= close.showAndWait();
+        ButtonType button=result.orElse(ButtonType.CANCEL);
+        if(button==ButtonType.OK)
+        {
+            System.out.println("Dong cua so");
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        else
+        {
+            close.close();
+        }
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
